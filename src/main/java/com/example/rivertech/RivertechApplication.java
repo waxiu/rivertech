0fe49c007807034a45055be31dbfeb0ff5bee473
IVVO\ivoja <ivojakubowski@gmail.com>
2024-11-24 17:10:13 +0100

    init commit

---
+++ src/main/java/com/example/rivertech/RivertechApplication.java
@@ -0,0 +1,13 @@
+package com.example.rivertech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RivertechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RivertechApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/rivertech/RivertechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rivertech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RivertechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
